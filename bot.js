31cb77a58e8e9d18beccfd69e10fad96a52b77fd
12345Hosin <51540395+12345Hosin@users.noreply.github.com>
2019-06-29 18:59:56 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "+";
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Cyhper Bot Script By : DREAM`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`TEXT`,"http://twitch.tv/Dream")
+client.user.setStatus("dnd")
+});
+
+client.login('NTg1OTQ2MDE4MDM0NjE0Mjgx.XRNWHQ.ClH3Rx7Jyo0JD4ck2yh143NrykQ'
+);// لا تغير فيها شيء
